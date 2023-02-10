8235cc67f3e29eef1ff80a3addd1e1bcd52c1797
Graviton23 <90819083+Graviton23@users.noreply.github.com>
2023-02-10 12:32:12 +0530

    All_Digitcount.java

---
+++ day-4/All_Digitcount.java
@@ -0,0 +1,10 @@
+import java.io*;
+import java.util.*;
+class All_Digitcount
+{
+   public int allDigitsCount(int input1)
+{
+   String str=Integer.toString(input1);
+   return str.length();
+  }
+}
